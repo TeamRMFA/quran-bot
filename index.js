06210d767bae1b4e4387783b291bcc9f54c94bf9
Mort The Spoon <52796888+mortada-killer0@users.noreply.github.com>
2021-06-25 01:57:11 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+app.listen(() => console.log(" Bird YT start "));
+app.use('/ping', (req, res) => {
+    res.send(new Date());
+});
+const { TOKEN, CHANNEL, SERVER, STATUS, LIVE } = require("./config.json");
+const discord = require("discord.js");
+const client = new discord.Client();
+const ytdl = require("ytdl-core");
+///////بوت قرأن كريم by raoof
+client.on("ready", async () => {
+    client.user.setActivity(STATUS + "")
+    let channel = client.channels.cache.get(CHANNEL) || await client.channels.fetch(CHANNEL)
+    if(!channel) return;
+    const connection = await channel.join();
+    connection.play(ytdl(LIVE))
+})
+setInterval(async function() {
+    if(!client.voice.connections.get(SERVER)) {
+        let channel = client.channels.cache.get(CHANNEL) || await client.channels.fetch(CHANNEL)
+        if(!channel) return;
+        const connection = await channel.join()
+        connection.play(ytdl(LIVE))
+    }
+}, 20000)
+client.login(TOKEN)
